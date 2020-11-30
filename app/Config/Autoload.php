4d4513799a9b1682aa85c9e66bca3196afdac3d6
EMMANUEL TOWOJU <hi@towoju.com>
2020-11-30 04:28:36 +0100

    first commit

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -42,7 +42,7 @@ class Autoload extends AutoloadConfig
 	public $psr4 = [
 		APP_NAMESPACE => APPPATH, // For custom app namespace
 		'Config'      => APPPATH . 'Config',
-		'Flutterwave' => ROOTPATH . 'vendor/flutterwavedev/flutterwave/library',
+		'Flutterwave' => ROOTPATH . 'flutterwave/library',
 	];
 
 	/**
